c53ff0d2ecec949aa596baa3256094c46926ccb0
Zip <76966589@qq.com>
2018-06-19 16:02:44 +0800

    增加图标

--- 自动关机Cmd/Form1.Designer.cs
+++ 自动关机Cmd/Form1.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.rabShutdown3 = new System.Windows.Forms.RadioButton();
             this.rabShutdown2 = new System.Windows.Forms.RadioButton();
@@ -282,7 +283,8 @@
             this.ClientSize = new System.Drawing.Size(376, 131);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
